4401c311d04c17dc7e705dc8ee0e77f5ec38a4fa
Jenyffer Prochno <jlprochno@gmail.com>
2024-02-09 09:53:02 -0300

    Publishing

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace carPoo;
+
+class Carro
+{
+    private string marca = "Fiat";
+    private string modelo = "UNO Mille";
+    private string anoFabricacao = "2015";
+    private string cor = "Branco";
+    private string tipo = "o melhor carro do mundo!";
+
+    public void CarDescription(){
+        Console.WriteLine($"{marca} {modelo} {cor}, é {tipo}");
+    }
+
+    public void Ligar(){
+        Console.WriteLine($"O {modelo} está ligado!");
+    }
+    public void Desligar(){
+        Console.WriteLine($"O {modelo} está desligado!");
+    }
+    
+}
+
